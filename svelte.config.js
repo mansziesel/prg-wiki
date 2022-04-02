e6fb2a1e5981a9ea2cc10670c0ad7b72c74da7f1
mansziesel <mansziesel@gmail.com>
2022-04-02 13:45:37 +0200

    remove vercel.json

--- svelte.config.js
+++ svelte.config.js
@@ -3,24 +3,13 @@ import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
-		browser: {
-			// hydrate: false,
-			// router: false
-		},
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
+		adapter: adapter(),
 		vite: {
 			optimizeDeps: {
 				include: ['highlight.js', 'highlight.js/lib/core']
 			}
 		},
 		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
 		}
 	}
