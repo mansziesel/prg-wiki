d86019190fd00f1c0a3a14c4673be33130b2a1c2
Mans Ziesel <mansziesel@gmail.com>
2022-05-12 18:13:58 +0100

    add scss

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,8 @@
 <script>
+	import '../app.scss';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import '../global.css';
-	import 'highlight.js/styles/atom-one-dark.css';
+	import 'highlight.js/styles/github-dark.css';
 </script>
 
 <Navbar />
@@ -10,12 +10,3 @@
 	<slot />
 </main>
 <Footer />
-
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
-
-	pre,
-	code {
-		font-family: 'JetBrains Mono', monospace !important;
-	}
-</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess';
 // svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 
@@ -7,10 +8,26 @@ export default {
 		vite: {
 			optimizeDeps: {
 				include: ['highlight.js', 'highlight.js/lib/core']
+			},
+
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@use "src/variables.scss" as *;'
+					}
+				}
 			}
 		},
 		prerender: {
 			default: true
 		}
-	}
+	},
+
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;'
+			}
+		})
+	]
 };
