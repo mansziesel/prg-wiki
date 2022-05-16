0a87d80eb04f5b02491962b7777136ce666b3196
Kevin Tieman <kevintieman@outlook.com>
2022-05-16 09:19:15 +0200

    wrapper & font-color

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,8 +5,22 @@
 	import 'highlight.js/styles/github-dark.css';
 </script>
 
+<div id="wrapper">
 <Navbar />
-<main class="max-w-3xl mx-auto">
+<main>
 	<slot />
 </main>
 <Footer />
+</div>
+
+<style>
+
+#wrapper {
+	background-color: rgb(100, 100, 100);
+	max-width: 1000px;
+	min-height: 100vh;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+</style>
